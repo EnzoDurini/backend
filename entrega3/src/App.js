89837491f1f93df6871d5ef922875a44b175208a
EnzoDurini <89779627+EnzoDurini@users.noreply.github.com>
2022-12-27 21:24:49 -0300

    Update App.js

--- entrega3/src/App.js
+++ entrega3/src/App.js
@@ -1,35 +1,41 @@
-import express, { response } from 'express'
-import ProductManager from './ProductManager'
+import express from 'express'
+import ProductManager from './ProductManager.js'
 const app = express()
 app.use(express.urlencoded({extended:true}))
 
 app.get('/', (request,response)=>{
-    response.send('Hi, how are You?')
+    response.send('Hi, how are You?')})
 
-})
-
-app.get('/products', (request, response )=>{
-    let limit = parseInt(request.query.limit)
+app.get('/products', async (req, res )=>{
 try {
-    if (limit === 0 || !limit){
-        response.json(ProductManager.getProducts())}
-        else{
-            const arrayProducts = ProductManager.getProducts()
-            let arrayLimit = arrayProducts.slice(0,limit)
-        }
+    let limit = req.query.limit
+    if(limit != null || limit > 0) {
+        const products = await ProductManager.getProducts();
+        const productsLimit = products.slice(0, limit);
+        res.json(productsLimit);
+    }else{
+        const products = await ProductManager.getProducts();
+        res.json(products);
+    }
 } catch (error) {
     res.send(console.log("Error: ", error))
 }
 })
 
-app.get('/products/:pid', async(req,res) =>{
-    let pid = parseInt(req.params.pid);
-    let response = await ProductManager.getElementById(pid)
-    console.log(response)
-    res.json(response || {"Error": "product not found"})
+app.get('/products/:pid', async (req,res) =>{
 
-})
+    try { 
+        let {pid} = req.params;
+        console.log(pid)
+        let product = await ProductManager.getProductById(parseInt(pid))
+        res.json(product)
+        
+    } catch (e) {
+        res.send(console.log("Error:", e))
+    }})
 
 app.get('*', (req,res)=>{
     res.send('Page not found')
 })
+
+app.listen(8080, ()=> {console.log('Server running on port', 8080);})
